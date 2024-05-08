c98e4ea116c9a515ce283658e2fe970581b799f8
Me <Me@134.36.36.99>
2024-05-08 15:33:36 +0100

    New comment

--- src/main/java/prt/springbootthymeleafcrudwebapp/SpringbootThymeleafCrudWebAppApplication.java
+++ src/main/java/prt/springbootthymeleafcrudwebapp/SpringbootThymeleafCrudWebAppApplication.java
@@ -2,7 +2,7 @@ package prt.springbootthymeleafcrudwebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//Test
 @SpringBootApplication
 public class SpringbootThymeleafCrudWebAppApplication {
 
